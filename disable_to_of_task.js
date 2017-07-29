8608401108bbc2dcea07932aacd752f0af8554f0
fukumame <tanuma0115@gmail.com>
2017-07-29 21:11:26 +0900

    Add a button to shorten TO mention at message input area.

--- disable_to_of_task.js
+++ disable_to_of_task.js
@@ -1,10 +1,24 @@
-var buttonHTML = '<input type="button" value="TO/RE 無効化" />';
-var replaceToText = function () {
-  var taskContentTextArea = $('#_taskNameInput');
-  var orgText = taskContentTextArea.val();
-  var replacedText = orgText.replace(/\[To\:.*\]/g, 'TO ');
-  replacedText = replacedText.replace(/\[返信.*\]/g, 'RE ')
-  taskContentTextArea.val(replacedText);
-}
-$(buttonHTML).appendTo('div.addTaskContainer__submitContainer')
-.click(replaceToText);
+$(function(){
+  var buttonHTML = '<input type="button" value="TO/RE 無効化" />';
+  var replaceToText = function () {
+    var taskContentTextArea = $('#_taskNameInput');
+    var orgText = taskContentTextArea.val();
+    var replacedText = orgText.replace(/\[To\:.*\]/g, 'TO ');
+    replacedText = replacedText.replace(/\[返信.*\]/g, 'RE ')
+    taskContentTextArea.val(replacedText);
+  };
+  $(buttonHTML).appendTo('div.addTaskContainer__submitContainer').click(replaceToText);
+});
+
+$(function(){
+  var buttonHTML = '<input type="button" value="TO省略化" />';
+  var shortenToDescription = function () {
+    var messageArea = $('#_chatText');
+    var orgMessage = messageArea.val();
+    var replacedMessage = orgMessage.replace(/(\[To\:[0-9]+\])(.+)(\n)/mg, '$1');
+    // 最後に改行を入れる
+    replacedMessage = replacedMessage.replace(/(\[To\:.+\])/, '$1\n');
+    messageArea.val(replacedMessage)
+  };
+  $(buttonHTML).prependTo('div.chatInput__submitContainer').click(shortenToDescription)
+});
