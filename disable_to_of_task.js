12f517c0e0f58d9149ed15bac643a6e3b2bf5d06
fukumame <tanuma0115@gmail.com>
2017-09-11 23:17:41 +0900

    Add a configuration for chrome extension.

--- disable_to_of_task.js
+++ disable_to_of_task.js
@@ -1,28 +1,35 @@
-$(function(){
+const scriptContent=
+`
+(function(){
   var buttonHTML = '<input type="button" value="TO/REを無効化" class="button" />';
   var replaceToText = function () {
     var taskContentTextArea = $('#_taskNameInput');
     var orgText = taskContentTextArea.val();
-    var replacedText = orgText.replace(/\[To\:.*\]/g, 'TO ');
-    replacedText = replacedText.replace(/\[返信.*\]/g, 'RE ');
-    replacedText = replacedText.replace(/\[rp.*\]/g, 'RE ');
+    var replacedText = orgText.replace(/\\[To\\:.*\\]/g, 'TO ');
+    replacedText = replacedText.replace(/\\[返信.*\\]/g, 'RE ');
+    replacedText = replacedText.replace(/\\[rp.*\\]/g, 'RE ');
     taskContentTextArea.val(replacedText);
   };
   $(buttonHTML).appendTo('div.addTaskContainer__submitContainer')
     .click(replaceToText)
     .css({'margin-top':'5px'});
-});
+})();
 
-$(function(){
+(function(){
   var buttonHTML = '<input type="button" value="TOを短縮する" class="button" />';
   var shortenToDescription = function () {
     var messageArea = $('#_chatText');
     var orgMessage = messageArea.val();
-    var replacedMessage = orgMessage.replace(/(\[To\:[0-9]+\])(.+)(\n)/mg, '$1');
+    var replacedMessage = orgMessage.replace(/(\\[To\\:[0-9]+\\])(.+)(\\n)/mg, '$1');
     // 最後に改行を入れる
-    replacedMessage = replacedMessage.replace(/(\[To\:.+\])/, '$1\n');
+    replacedMessage = replacedMessage.replace(/(\\[To\\:.+\\])/, '$1\\n');
     messageArea.val(replacedMessage)
   };
   $(buttonHTML).prependTo('div.chatInput__submitContainer')
     .click(shortenToDescription)
-});
+})();
+`;
+
+var script = document.createElement("script");
+script.textContent = scriptContent;
+document.body.appendChild(script);
