024e923c388d85dfeed9dc9f7ebc264e7519a0af
fukumame <tanuma0115@gmail.com>
2018-04-05 16:48:39 +0900

    fix bugs

--- disable_to_of_task.js
+++ disable_to_of_task.js
@@ -1,35 +1,25 @@
-const scriptContent=
-`
-(function(){
-  var buttonHTML = '<input type="button" value="TO/REを無効化" class="button" />';
-  var replaceToText = function () {
-    var taskContentTextArea = $('#_taskNameInput');
-    var orgText = taskContentTextArea.val();
-    var replacedText = orgText.replace(/\\[To\\:.*\\]/g, 'TO ');
-    replacedText = replacedText.replace(/\\[返信.*\\]/g, 'RE ');
-    replacedText = replacedText.replace(/\\[rp.*\\]/g, 'RE ');
-    taskContentTextArea.val(replacedText);
-  };
-  $(buttonHTML).appendTo('div.addTaskContainer__submitContainer')
-    .click(replaceToText)
-    .css({'margin-top':'5px'});
-})();
+const buttonHTML = '<input type="button" value="TO/REを無効化" class="button" />';
+const replaceToText = function () {
+  const taskContentTextArea = $('#_taskNameInput');
+  const orgText = taskContentTextArea.val();
+  let replacedText = orgText.replace(/\[To\:.*\]/g, 'TO ');
+  replacedText = replacedText.replace(/\[返信.*\]/g, 'RE ');
+  replacedText = replacedText.replace(/\[rp.*\]/g, 'RE ');
+  taskContentTextArea.val(replacedText);
+};
+$(buttonHTML).appendTo('div.addTaskContainer__submitContainer')
+  .click(replaceToText)
+  .css({'margin-top':'5px'});
 
-(function(){
-  var buttonHTML = '<input type="button" value="TOを短縮する" class="button" />';
-  var shortenToDescription = function () {
-    var messageArea = $('#_chatText');
-    var orgMessage = messageArea.val();
-    var replacedMessage = orgMessage.replace(/(\\[To\\:[0-9]+\\])(.+)(\\n)/mg, '$1');
-    // 最後に改行を入れる
-    replacedMessage = replacedMessage.replace(/(\\[To\\:.+\\])/, '$1\\n');
-    messageArea.val(replacedMessage)
-  };
-  $(buttonHTML).prependTo('div.chatInput__submitContainer')
-    .click(shortenToDescription)
-})();
-`;
 
-var script = document.createElement("script");
-script.textContent = scriptContent;
-document.body.appendChild(script);
+const shortenButtonHTML = '<input type="button" value="TOを短縮する" class="button" />';
+const shortenToDescription = function () {
+  const messageArea = $('#_chatText');
+  const orgMessage = messageArea.val();
+  let replacedMessage = orgMessage.replace(/(\[To\:[0-9]+\])(.+)(\n)?/mg, '$1');
+  // 最後に改行を入れる
+  replacedMessage = replacedMessage.replace(/(\[To\:.+\])/, '$1\n');
+  messageArea.val(replacedMessage)
+};
+$(shortenButtonHTML).prependTo('div.chatInput__submitContainer')
+  .click(shortenToDescription);
